cb6d13d545264da79f5171b514c3d17905c62ec3
James Racine <jracine@bullhorn.com>
2018-04-09 16:18:36 -0400

    Removed extra images. using matrix for setting transformations

--- interacttest.js
+++ interacttest.js
@@ -1,6 +1,19 @@
 //const interact = require('interactjs');
 function init(){
-
+    /*
+    console.log(Math.cos(1.5707963268).toPrecision(6));
+    console.log(Math.sin(1.5707963268).toPrecision(6));
+    console.log(Math.cos(2.3561944902).toPrecision(6));
+    console.log(Math.sin(2.3561944902).toPrecision(6));
+    console.log(Math.cos(3.1415926536).toPrecision(6));
+    console.log(Math.sin(3.1415926536).toPrecision(6));
+    console.log(Math.cos(3.926990817).toPrecision(6));
+    console.log(Math.sin(3.926990817).toPrecision(6));
+    console.log(Math.cos(4.7123889804).toPrecision(6));
+    console.log(Math.sin(4.7123889804).toPrecision(6));
+    console.log(Math.cos(5.4977871438).toPrecision(6));
+    console.log(Math.sin(5.4977871438).toPrecision(6));
+    */
 }
 
 document.getElementById('Close').addEventListener('click',closeWindow);
@@ -43,8 +56,102 @@ interact('.dragandrotate')
   .on('doubletap', function (event) {
     changeImage();
   });
+//https://www.khanacademy.org/math/linear-algebra/matrix-transformations/lin-trans-examples/v/linear-transformation-examples-rotations-in-r2
+//Rot (theta) (X->) = [cos (theta) -sin(theta)]
+                    //[sign (theta) cos(theta)]
+                    //for 45 they eval to sqrt(2)/2, -sqrt(2)/2, 
+                    //                    sqrt(2)/2   sqrt(2)/2 
 
   function changeImage() {
+    var target = event.target;
+    var current_transform = window.getComputedStyle(target).transform;
+    var degrees = (parseFloat(target.getAttribute('rotate')) || 0) + 45;
+    if (degrees == 360){
+        degrees = 0;
+    }
+    var rotMat = getRotationMatrix(degrees);
+    var matrix = "";
+    if (current_transform != "none"){
+        var temp = current_transform.match(/[\d\.\-]+/g);    
+        matrix = "matrix("+ rotMat[0]+","+rotMat[1]+","+rotMat[2]+","+rotMat[3]+","+temp[4]+","+temp[5]+")";
+    }else{
+        matrix = "matrix("+rotMat[0]+","+rotMat[1]+","+rotMat[2]+","+rotMat[3]+",0,0)";    
+    }
+    //[\d\.\-]+
+    target.style['transform'] = matrix;
+    target.style['-webkit-transform'] = matrix;
+    target.setAttribute('rotate', degrees);
+  }
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    var current_transform = window.getComputedStyle(target).transform;
+    var matrix = "";
+    if (current_transform != "none"){
+        console.log(current_transform);
+        var temp = current_transform.match(/[\d\.\-]+/g);
+        console.log(temp);
+        matrix ="matrix("+temp[0]+","+temp[1]+","+temp[2]+","+temp[3]+","+x+","+y+")";
+    }else{
+        matrix ="matrix(1,0,0,1,"+x+","+y+")";  
+    }
+    
+    target.style.webkitTransform =
+    target.style.transform =
+      //'translate(' + x + 'px, ' + y + 'px)';
+      matrix;
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+
+function getRadians(degrees){
+    //should have worked, too much overflow
+    var theta = +((degrees * (Math.PI / 180)).toPrecision(6));
+}
+
+function getRotationMatrix(degrees){
+    var rotMat = []
+    switch(degrees) {
+        case 0:
+            rotMat = [1,0,0,1];
+            break;
+        case 45:
+            rotMat = [0.707107,-0.707107,0.707107,0.707107];
+            break;
+        case 90:
+            rotMat = [0,-1,1,0];
+            break;
+        case 135:
+            rotMat = [-0.707107,-0.707107,0.707107,-0.707107];
+            break;
+        case 180:
+            rotMat = [-1,0,0,-1];
+            break;
+        case 225:
+            rotMat = [-0.707107,0.707107,-0.707107,-0.707107];
+            break;
+        case 270:
+            rotMat = [0,1,-1,0];
+            break;
+        case 315:
+            rotMat = [0.707107,0.707107,-0.707107,0.707107];
+            break;
+    }
+    return rotMat;
+} 
+
+/*
+  function oldChangeImage() {
     var target = event.target;
     var str = event.target.src;
     var rotate = 0;
@@ -58,23 +165,4 @@ interact('.dragandrotate')
     str = str.replace(/(\d)/g, rotate) 
     target.src = str;
   }
-
-  function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
-
-  // this is used later in the resizing and gesture demos
-  window.dragMoveListener = dragMoveListener;
- 
+*/
